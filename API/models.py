e123e0d557fe91cfe54979be53cb6026045070c3
Afzal Hussain <azzu01122@gmail.com>
2022-04-06 12:49:03 +0530

    initial commit

---
+++ API/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    student_name = models.CharField(max_length=100)
+    roll_number = models.IntegerField()
+    email = models.EmailField(max_length=100)
+    subject = models.CharField(max_length=70) 
+    assignment = models.FileField(upload_to='pdfs/')
+    date = models.DateField()
