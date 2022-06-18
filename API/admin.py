e8465850797c9ffff04efe43bc2e60b4ab26e0bc
Afzal Hussain <azzu01122@gmail.com>
2022-06-18 09:04:33 +0530

    modify authentication system

--- API/admin.py
+++ API/admin.py
@@ -4,4 +4,4 @@ from .models import Student
 # Register your models here.
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'student_name','roll_number', 'email', 'assignment', 'date')
+    list_display = ['id', 'student_name','roll_number', 'email', 'assignment', 'date']
--- API/views.py
+++ API/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [BasicAuthentication]
-    Permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
